849c0073fd08ffed8b23295f7653dd19f488c84e
Harry_Clipston <56134588+HarryClipston@users.noreply.github.com>
2021-01-23 15:54:56 +0000

    cat_video

--- video_merger.py
+++ video_merger.py
@@ -20,15 +20,14 @@ def my_hook(d):
 
         # ffmpeg -stream_loop -1 -i bg.mp4 -i main.mp4 -filter_complex "[0][1]overlay=shortest=1[v]" -map "[v]" -map 1:a -c:a copy output.mp4
 
-        looping = ffmpeg.input("tom.mp4", stream_loop= -1) # Import tom as a looping stream, tom is 426x240
-        looping = ffmpeg.filter(looping, "colorkey", color="0x14FF43", similarity=0.3, blend=0.2) # This green I got myself from the tom video
-        stream = ffmpeg.input(filename, ss=20, t=20) # Get start at 20s in and make the clip 20s
+        looping = ffmpeg.input("cat.mp4", stream_loop= -1) # Import tom as a looping stream, tom is 426x240
+        looping = ffmpeg.filter(looping, "colorkey", color="0x2bd71c", similarity=0.3, blend=0.2) # This green I got myself from the tom video
+        stream = ffmpeg.input(filename, ss=30, t=30) # Get start at 20s in and make the clip 20s
         video = stream.video
         audio = stream.audio
         video = ffmpeg.filter(video, 'scale', 1280,720) # Resize to 720p
         video = ffmpeg.overlay(stream, looping, shortest=1, y="140")
         stream = ffmpeg.output(video, audio, 'test.mp4').overwrite_output()
-        print("output:", filename)
         stream.run()
 
 
