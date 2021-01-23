d5ef652011241150d5042108f34ef0f5eeb879f5
Harry <harrybest800@gmail.com>
2021-01-23 21:02:25 +0000

    split into downloader and renderer classes

---
+++ backend/downloader.py
@@ -0,0 +1,34 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        filename = d['filename']
+
+def download_video(url):
+
+    ydl_opts = {
+    'format': '[filesize<40M]', # File size seems to need to be tweaked - look into it more
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+}
+
+
+url = input("Please enter a youtube URL: ")
+#url = "https://www.youtube.com/watch?v=gTWz-zy3re4"
+
+#try:
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
+#except:
+    #print("error: try a shorter video")
